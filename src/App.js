bba9618cbe1b40890c1efb89c83c5d25baecf1b1
Hjarambulo <hj.arambulo92@gmail.com>
2021-05-25 19:07:12 -0300

    creado el componente footer y sidebar

--- src/App.js
+++ src/App.js
@@ -4,26 +4,35 @@ import './assets/css/App.css';
 // Importar componentes
 import Header from './components/Header';
 import Slider from './components/Slider'
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Slider/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="center">
+        <section id="content">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </section> 
+        <Sidebar/>
+      </div> 
+      <div class="clearfix"></div>
+      <Footer/>
     </div>
   );
 }
